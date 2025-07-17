0533dbc5d032fa9eaec2f3365581b3d3a3e08594
Gentle <68044078+ramirezg-web@users.noreply.github.com>
2025-07-16 22:18:38 -0400

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,6 +25,11 @@ interface UserSubscription {
   current_period_end: number;
   cancel_at_period_end: boolean;
 }
+interface UserSubscription {
+  subscription_status: string;
+  current_period_end: number;
+  cancel_at_period_end: boolean;
+}
 export default function RootLayout() {
   useFrameworkReady();
   const [session, setSession] = useState<Session | null>(null);
